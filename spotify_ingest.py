58fc0b6e634c64c5b5e5404c1dd4b2e394ab9954
bbudickk <mr.budik.003@gmail.com>
2026-09-06 19:45:55 +0300

    Created three functions for every stage: extract, transform and load.

--- spotify_ingest.py
+++ spotify_ingest.py
@@ -1,10 +1,11 @@
-import time
-import spotipy 
+""" Import system library to writing json"""
+import os
+import json
+
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.exceptions import SpotifyException
 from dotenv import load_dotenv
-import os
-import json
 
 load_dotenv()
 
@@ -19,37 +20,75 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri=client_uri,
     scope="user-library-read"))
 
-def load_favorite_tracks(rate_limit: int):
-    track_dict = []
+
+def extract_favorite_tracks(page_size: int):
+    """
+        Extract favorites tracks from user library. 
+        Parametrs: 
+        - page_size - get 1 to 20 tracks per query.
+        
+        Return:
+        - track_list - full tracklist without any formatting.
+    """
     try:
-        results = sp.current_user_saved_tracks(limit=rate_limit)
+        extracted_tracklist = sp.current_user_saved_tracks(limit=page_size)
     except SpotifyException as e:
-        results = None
+        extracted_tracklist = None
         print(f"That happend: {e}")
+    track_list = []
 
-    track_count = 1
-    while results:
-        for idx, item in enumerate(results['items']):
+    while extracted_tracklist:
+        for item in extracted_tracklist['items']:
             track = item['track']
-            print(track_count + idx, track['artists'][0]['name'], " – ", track['name'])
-            track_dict.append(dict(
-                track_id = track_count + idx,
-                artist_name = track['artists'][0]['name'], 
-                track_name = track['name'],
-                release_date = track['album']['release_date'],
-                track_uri=track['uri'],
-            ))
-
-        if results['next']:
-            results = sp.next(results)
-            track_count += idx + 1
-            time.sleep(0.5)
+            track_list.append(track)
+        if extracted_tracklist['next']:
+            extracted_tracklist = sp.next(extracted_tracklist)
         else:
-            results = None
-            with open('data/favorite_tracks.json', 'w', encoding="utf-8") as file:
-                    json.dump(track_dict, file, ensure_ascii=False, indent=4)
-                    print("Tracks was sucessfully write!")
+            extracted_tracklist = None
 
-load_favorite_tracks(20)
+    return track_list
 
-    
+
+def transform_favorite_tracks(track_list):
+    """
+        Transform tracklist to suit requirements. 
+        Parametrs: 
+        - track_list - list without any transformations.
+        
+        Return:
+        - formated_tracklist - only neseccary data about favorite tracks.
+    """
+    formated_tracklist = []
+
+    for track in track_list:
+        formated_tracklist.append(
+            {
+                "artist_name": track['artists'][0]['name'], 
+                "track_name":  track['name'],
+                "release_date": track['album']['release_date'],
+                "track_uri": track['uri']
+            }
+        )
+
+    return formated_tracklist
+
+
+def load_favorite_tracks(path: str, formated_tracklist: list):
+    """
+        Load json tracklist into file. 
+        Parametrs: 
+        - path - file path.
+        - json_tracklist - complete tracklist in json format.
+    """
+    if formated_tracklist:
+        with open(path, 'w', encoding="utf-8") as file:
+            json.dump(formated_tracklist, file, ensure_ascii=False, indent=4)
+            print("Tracks was sucessfully write!")
+    else:
+        print("Tracklist is empty!")
+
+
+
+final_track_list = extract_favorite_tracks(20)
+final_formated_tracklist = transform_favorite_tracks(final_track_list)
+load_favorite_tracks('data/favorite_tracks.json', final_formated_tracklist)
